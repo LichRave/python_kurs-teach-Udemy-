1ec845fb433aae0defc0b2aa0fffa531c35efb0c
Artem Monkiewicz <artem.monkiewicz@gmail.com>
2024-02-05 17:43:58 +0100

    training from scratch, day 14 (05.02.24)

--- Powrot.py
+++ Powrot.py
@@ -46,3 +46,129 @@ lst124 = []
 
 for cyfra in lst123:
     lst124.append
+
+
+# %%
+def print_communique():
+    print("Pierwsza linijka komunikatu")
+    print("Druga linijka komunikatu")
+    print("Trzecia linijka komunikatu")
+
+
+a = 24
+
+if a / 2 == 0:
+    b = a / 2
+    print_communique()
+else:
+    b = a / 3
+
+# Gdzieś dalej w kodzie
+
+if b != 1:
+    print_communique()
+
+
+# %%
+def sum_up(x, y):
+    return x + y
+
+
+a = sum_up(3, 4)
+b = sum_up(2.5, 7.5)
+c = sum_up(4, -2)
+
+print(a)
+print(b)
+print(c)
+
+
+# %%
+class Pies:
+    def szczekaj(self):
+        print("Hau, hau!")
+
+    def __init__(self, name):
+        self.name = name
+
+    def toaleta(self):
+        print("Siusiu")
+
+
+pies1 = Pies("Burek")
+pies1.name
+pies1.szczekaj()
+pies1.toaleta()
+
+
+class Pies:
+    def __init__(self, name, is_hungry):
+        self.name = name
+        self.is_hungry = is_hungry
+
+    def jedz(self):
+        if self.is_hungry == True:
+            self.is_hungry = False
+
+
+# %%
+lst1 = [3, 7, 6, 8, 9, 11, 15, 25]
+lst2 = []
+
+for i in lst1:
+    lst2.append(i**2)
+
+print(lst2)
+
+# %%
+lst1 = [111, 32, -9, -45, -17, 9, 85, -10]
+lst2 = []
+
+for i in lst1:
+    if i > 0:
+        lst2.append(i)
+
+print(lst2)
+
+# %%
+dict = {
+    " z1 ": 900,
+    " t1 ": 1100,
+    " p1 ": 2300,
+    " r1 ": 1050,
+    " k1 ": 3200,
+    " g1 ": 400,
+}
+lst = []
+
+for value in dict.values():
+    if value > 1000:
+        lst.append(value - 1000)
+
+print(lst)
+
+# %%
+lst1 = [3.14, 66, " Teddy Bear ", True, [], {}]
+lst3 = []
+
+for i in lst1:
+    lst3.append(type(i))
+
+print(lst3)
+
+
+# %%
+lst1 = [1, 2, 3, 4, 5]
+lst2 = [i for i in lst1]
+
+print(lst2)
+
+# %%
+
+rng = [i for i in range(1200, 2001, 130)]
+
+
+print(rng)
+
+
+# %%
